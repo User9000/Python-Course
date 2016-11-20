83d5ff12c4a479a48cf6c83a45fb4265a580ab11
Carlo Lam <Carlo@Carlos-MacBook-Pro.local>
2016-11-19 18:39:45 -0800

    another script

--- Script8.py
+++ Script8.py
@@ -5,12 +5,13 @@ file = open("example3.txt","w")
 def c_to_f(c):
     if c < -273.15:
         return "That temperature does not make sens!"
-    else:
+    #else:
         f=c*9/5 +32
         return f
 
 
 for item in temperatures:
+    
     result = c_to_f(item)
     file.write("Celcius: "+str(result)+"\n")
 
