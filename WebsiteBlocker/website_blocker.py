2aa0f9b21a731c613422b47f8a0363d13017eb2c
Carlo Lam <Carlo@Carlos-MacBook-Pro.local>
2016-11-27 15:59:39 -0800

    new script from the course

--- WebsiteBlocker/website_blocker.py
+++ WebsiteBlocker/website_blocker.py
@@ -1,20 +1,44 @@
 
 
+#this program will prevent you from accessing the sites in the website_list
 import time
 from datetime import datetime as dt
 
-
+hosts_temp="hosts"
 hosts_path='/etc/hosts'
 redirect='127.0.0.1'
 
-
 website_list=['www.facebook.com','facebook.com','www.outlook.com','www.cnn.com','www.nytimes.com','www.aristeguinoticias.com','www.lacronica.com']
 
-
+#to run your program as a daemon
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) <dt.now()< dt(dt.now().year,dt.now().month,dt.now().day,18):
+    #if dt(dt.now().year, dt.now().month, dt.now().day, 0) <dt.now()< dt(dt.now().year,dt.now().month,dt.now().day,1):
+       if dt(dt.now().year, dt.now().month, dt.now().day, 17) <dt.now()< dt(dt.now().year,dt.now().month,dt.now().day,19):
         print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass 
+                else:
+                    file.write(redirect+"  "+website+"\n")
+                  
+       else:
+        #read all lines of the files/ this will move the pointer to the last line
+        with open(hosts_path,'r+') as file:
+            #get all lines as a list ans pass them to content variable
+            content=file.readlines()
+            #take the pointer back to the beginning
+            file.seek(0)
+            #iterate through the list
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            #erase everything below the pointer
+            file.truncate()
+        print("Non working hours...")
 
-    else:
-         print("Non working hours...")
-    time.sleep(5)
+
+       
+    
+       time.sleep(5)
