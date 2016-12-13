a3ecf3e1b2f2530c365ab7aeaba7924f4155eb34
Carlo Lam <calam12@gmail.com>
2016-12-12 18:21:26 -0800

    playing with cv2
    playing with cv2

--- opencv/script1.py
+++ opencv/script1.py
@@ -7,6 +7,10 @@ print(img)
 print(img.shape)
 print(img.ndim)
 
-cv2.imshow("Penguins",img)
-cv2.waitKey(2000)
+resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("Penguins",resized_image)
+cv2.imwrite("Penguins_Resized.jpg", resized_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
