3d4aab510c0c5a27912cc2b3771f105226e97f1c
Carlo Lam <calam12@gmail.com>
2016-12-12 16:50:50 -0800

    object oriented
    object oriented

--- OOP Program GUI/backend.py
+++ OOP Program GUI/backend.py
@@ -9,8 +9,7 @@ class Database:
 		self.cur=self.conn.cursor()
 		self.cur.execute("CREATE TABLE IF NOT EXISTS  books(id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer) ")
 		self.conn.commit()
-		#conn.close()
-
+		
 
 	def insert(self,title,author,year,isbn):
 		self.cur.execute("INSERT INTO books VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
@@ -20,28 +19,25 @@ class Database:
 	def view(self):
 		self.cur.execute("SELECT * FROM books")
 		rows=self.cur.fetchall()
-		
 		return rows
 
-	def search(self,title="",author="",year="",isbn=""):
-		
+	def search(self,title="",author="",year="",isbn=""):	
 		self.cur.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
 		rows=self.cur.fetchall()
-		
 		return rows
 
 
 	def delete(self,id):
-		
 		self.cur.execute("DELETE FROM books WHERE id=?",(id,))
 		self.conn.commit()
 		
 
-
-	def update(self,id,title,author,year,isbn):
-		
+	def update(self,id,title,author,year,isbn):	
 		self.cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
 		self.conn.commit()
+	
+	def __del__(self):
+		self.conn.close()
 		
 		  
 
